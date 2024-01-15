efa4f06da2c0e4f703665d70522326e00ea296d0
anaveroneze <alsolorzano@inf.ufsm.br>
2024-01-15 10:12:07 -0500

    putting code to jupyter notebook

--- code/main.py
+++ code/main.py
@@ -12,6 +12,7 @@ from datetime import datetime
 # 2. Identify long intervals between last read/write and a met operation open/close 
 # 3. Identify distance between the first rank to finish and others
 # 4. Identify long intervals between operations in the same rank
+
 class Job:
 
     def __init__(self, job, ranks, nodes, users, filename, exe):
@@ -35,92 +36,106 @@ class Job:
         print("Directory:", self.exe)
         # print("Operations filenames:", self.filename)
 
-def get_statistics(df, self):
+def get_statistics(df, output_file, self):
 
-    print("Modules collected:", df['module'].unique())
-    print("Module data (MOD):", list(df.type).count('MOD'))
-    print("Meta data (MET):", list(df.type).count('MET'))
-    exec_time = round(df['end'].max() - df['start'].min(), 5)
-    print("I/O execution time:", exec_time, "seconds")
-    print("Total bandwidth (MiB/second):", round((df['len'].sum()/exec_time)/(1024 ** 2), 2))
+    with open(output_file, 'w') as f:
 
-    df_rw = df[df['op'].isin(["read", "write"])]
-    df_read = df[df['op'] == "read"]
-    df_write = df[df['op'] == "write"]
+        def write_to_file(*args):
+            print(" ".join(map(str, args)), file=f, flush=True)
 
-    print("---------------------------------------")
-    print("EXECUTION SUMMARY PER APPLICATION PHASE:")
-    print("---------------------------------------")
+        write_to_file("---------------------------------------")
+        write_to_file("JOB CHARACTERISTICS:")
+        write_to_file("---------------------------------------")
+        write_to_file("Job ID:", self.job)
+        write_to_file(len(self.ranks), "Rank (s):", sorted(self.ranks))
+        write_to_file(len(self.ranks), "Node (s):", sorted(self.nodes))
+        write_to_file("Users:", self.users)
+        write_to_file("Directory:", self.exe)
 
-    current_op = None
-    phase_start = None
-    total_durations = {'read': 0, 'write': 0, 'open': 0, 'close': 0} 
+        write_to_file("Modules collected:", df['module'].unique())
+        write_to_file("Module data (MOD):", list(df.type).count('MOD'))
+        write_to_file("Meta data (MET):", list(df.type).count('MET'))
+        exec_time = round(df['end'].max() - df['start'].min(), 5)
+        write_to_file("I/O execution time:", exec_time, "seconds")
+        write_to_file("Total bandwidth (MiB/second):", round((df['len'].sum() / exec_time) / (1024 ** 2), 2))
 
-    def update_total_duration(op, phase_start, phase_end, length):
-        if current_op is not None and current_op == op:
-            total_durations[op] += (phase_end - phase_start)
+        df_rw = df[df['op'].isin(["read", "write"])]
+        df_read = df[df['op'] == "read"]
+        df_write = df[df['op'] == "write"]
 
-    for index, row in df.iterrows():
-        if current_op is None or current_op != row['op']:
-            update_total_duration(current_op, phase_start, row['end'], row['len'])
-            current_op = row['op']
-            phase_start = row['start']
+        write_to_file("---------------------------------------")
+        write_to_file("EXECUTION SUMMARY PER APPLICATION PHASE:")
+        write_to_file("---------------------------------------")
 
-    # Get the last phase
-    update_total_duration(current_op, phase_start, row['end'], row['len'])
+        current_op = None
+        phase_start = None
+        total_durations = {'read': 0, 'write': 0, 'open': 0, 'close': 0}
 
-    pivot_df = df.pivot_table(index=None, columns='op', values='len', aggfunc='sum')  
-    for op, duration in total_durations.items():
-        print(f'Duration for {op}s: {round(duration, 4)} seconds')
-        if op == "read" or op == "write":
-            bytesproc = round((pivot_df[op].max()/(1024 ** 2))/duration, 4)
-            print("Bandwidth:", bytesproc, "(MiB/second)")
+        def update_total_duration(op, phase_start, phase_end, length):
+            if current_op is not None and current_op == op:
+                total_durations[op] += (phase_end - phase_start)
 
-    print("\n")
-    print("READS (MiB):", round(df_read['len'].sum()/(1024 ** 2)))
-    print("Max MiB per rank:", round(df_read.groupby('rank')['len'].agg('sum').max() / (1024 ** 2)))
-    print("Min MiB per rank:", round(df_read.groupby('rank')['len'].agg('sum').min() / (1024 ** 2)))
-    print("Bandwidth (MiB/second):", round((df_read['len'].sum()/(df_read['end'].max() - df_read['start'].min()))/(1024 ** 2), 2))
-    print("\nWRITES (MiB):", round(df_write['len'].sum()/(1024 ** 2)))
-    print("Max MiB per rank:", round(df_write.groupby('rank')['len'].agg('sum').max() / (1024 ** 2)))
-    print("Min MiB per rank:", round(df_write.groupby('rank')['len'].agg('sum').min() / (1024 ** 2)))
-    print("Bandwidth (MiB/second):", round((df_write['len'].sum()/(df_write['end'].max() - df_write['start'].min()))/(1024 ** 2), 2))
+        for index, row in df.iterrows():
+            if current_op is None or current_op != row['op']:
+                update_total_duration(current_op, phase_start, row['end'], row['len'])
+                current_op = row['op']
+                phase_start = row['start']
 
-    # IMBALANCE METRICS:
-    # Average = sum time computing / number of ranks
-    # Imbalance time = time that would be saved if the load was perfectly balanced across resources
-    # Percent Imbalance = performance that could be gained if load was perfectly balanced
-    # Imbalance Percentage = percentage of time that resources (excluding the slowest one) are
-    # not involved in computing 
-    print("---------------------------------------")
-    print("LOAD IMBALANCE METRICS:")
-    print("---------------------------------------")
-    # Get difference between execution time and time processing I/O per rank
-    df_idle = df.groupby('rank')['dur'].sum().reset_index()
-    df_idle.columns = ['Rank', 'I/O Time']
-    df_idle['Total time - I/O Time'] = exec_time - df_idle['I/O Time']
-    df_idle = df_idle.sort_values(by='Total time - I/O Time', ascending=False)
+        # Get the last phase
+        update_total_duration(current_op, phase_start, row['end'], row['len'])
 
-    print("Total execution time:", exec_time)
-    num_ranks = df_idle['I/O Time'].nunique()
-    average = df_idle['I/O Time'].sum() / num_ranks
-    print("- Average:", round(average), "seconds")
-    it = df_idle['I/O Time'].max() - average
-    print("- Imbalance Time:", round(it, 2), "seconds")
-    pi = ((df_idle['I/O Time'].max() / average) - 1) * 100
-    print("- Percent Imbalance:", round(pi, 2), "%")
-    ip = (it / df_idle['I/O Time'].max()) * (num_ranks / (num_ranks - 1))
-    print("- Imbalance Percentage:", round(ip, 2), "%")
-    std = np.std(df_idle['I/O Time'])
-    print("- Standard deviation", round(std, 2))
+        pivot_df = df.pivot_table(index=None, columns='op', values='len', aggfunc='sum')
+        for op, duration in total_durations.items():
+            write_to_file(f'Duration for {op}s: {round(duration, 4)} seconds')
+            if op == "read" or op == "write":
+                bytesproc = round((pivot_df[op].max() / (1024 ** 2)) / duration, 4)
+                write_to_file("Bandwidth:", bytesproc, "(MiB/second)")
 
-    print("---------------------------------------")
-    print("PROFILE PER RANK: ")
-    print("---------------------------------------")
-    print("Total time without executing I/O operations:")
-    df['start'] = pd.to_datetime(df['start'], unit='s').dt.round('S')
-    df['end'] = pd.to_datetime(df['end'], unit='s').dt.round('S')
-    print(df_idle)
+        write_to_file("\n")
+        write_to_file("READS (MiB):", round(df_read['len'].sum() / (1024 ** 2)))
+        write_to_file("Max MiB per rank:", round(df_read.groupby('rank')['len'].agg('sum').max() / (1024 ** 2)))
+        write_to_file("Min MiB per rank:", round(df_read.groupby('rank')['len'].agg('sum').min() / (1024 ** 2)))
+        write_to_file("Bandwidth (MiB/second):", round((df_read['len'].sum() / (df_read['end'].max() - df_read['start'].min())) / (1024 ** 2), 2))
+        write_to_file("\nWRITES (MiB):", round(df_write['len'].sum() / (1024 ** 2)))
+        write_to_file("Max MiB per rank:", round(df_write.groupby('rank')['len'].agg('sum').max() / (1024 ** 2)))
+        write_to_file("Min MiB per rank:", round(df_write.groupby('rank')['len'].agg('sum').min() / (1024 ** 2)))
+        write_to_file("Bandwidth (MiB/second):", round((df_write['len'].sum() / (df_write['end'].max() - df_write['start'].min())) / (1024 ** 2),2))
+
+        # IMBALANCE METRICS:
+        # Average = sum time computing / number of ranks
+        # Imbalance time = time that would be saved if the load was perfectly balanced across resources
+        # Percent Imbalance = performance that could be gained if load was perfectly balanced
+        # Imbalance Percentage = percentage of time that resources (excluding the slowest one) are
+        # not involved in computing
+        write_to_file("---------------------------------------")
+        write_to_file("LOAD IMBALANCE METRICS:")
+        write_to_file("---------------------------------------")
+        # Get difference between execution time and time processing I/O per rank
+        df_idle = df.groupby('rank')['dur'].sum().reset_index()
+        df_idle.columns = ['Rank', 'I/O Time']
+        df_idle['Total time - I/O Time'] = exec_time - df_idle['I/O Time']
+        df_idle = df_idle.sort_values(by='Total time - I/O Time', ascending=False)
+
+        write_to_file("Total execution time:", exec_time)
+        num_ranks = df_idle['I/O Time'].nunique()
+        average = df_idle['I/O Time'].sum() / num_ranks
+        write_to_file("- Average:", round(average), "seconds")
+        it = df_idle['I/O Time'].max() - average
+        write_to_file("- Imbalance Time:", round(it, 2), "seconds")
+        pi = ((df_idle['I/O Time'].max() / average) - 1) * 100
+        write_to_file("- Percent Imbalance:", round(pi, 2), "%")
+        ip = (it / df_idle['I/O Time'].max()) * (num_ranks / (num_ranks - 1))
+        write_to_file("- Imbalance Percentage:", round(ip, 2), "%")
+        std = np.std(df_idle['I/O Time'])
+        write_to_file("- Standard deviation", round(std, 2))
+
+        write_to_file("---------------------------------------")
+        write_to_file("PROFILE PER RANK: ")
+        write_to_file("---------------------------------------")
+        write_to_file("Total time without executing I/O operations:")
+        df['start'] = pd.to_datetime(df['start'], unit='s').dt.round('S')
+        df['end'] = pd.to_datetime(df['end'], unit='s').dt.round('S')
+        write_to_file(df_idle)
 
     # # Iterate over each operation
     # for op in df_delay['op'].unique():
@@ -136,7 +151,6 @@ def get_statistics(df, self):
     #     # Append the results to the result DataFrame
     #     result_df = pd.concat([result_df, pd.DataFrame([{'op': op, 'rank_fast': rank_fast, 'rank_slow': rank_slow, 
     #         'time_fast': time_fast, 'time_slow': time_slow, 'diff': time_diff}])], ignore_index=True)
-
 
 def get_visualizations_R(filename, figname):
 
@@ -267,7 +281,8 @@ def main():
         # Job characteristics
         job.print_info()     
         # Job profile
-        get_statistics(local_df, job)
+        output_file = args.input.replace(".csv", ".txt")
+        get_statistics(local_df, output_file, job)
         # Job visualizations
         # get_visualizations_R(args.input, "./figures/ior/teste.png")
         # get_visualizations_py(local_df, "./figures/ior/teste.png")
